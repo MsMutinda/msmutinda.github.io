8fba3ba667e1926847aa92cb21cfefa4e82ace41
Julie <MsMutinda@users.noreply.github.com>
2021-03-13 22:38:40 +0300

    Animations

--- email.php
+++ email.php
@@ -4,56 +4,58 @@
         echo 'error: You need to submit the form';
     }
     //collect the data
-    $name = $_POST['name'];
-    $sender_email = $_POST['email'];
-    $message = $_POST['message'];
-    //validate - invalid fields
-    if(empty($name)||empty($sender_email)) {
-        echo "Please fill in your name and email address";
-        exit;
-    }
-    //validate - injected input
-    if(IsInjected($visitor_email)) {
-    echo "Bad email value!";
-    exit;
-    }
-    //set variables
-    $recipient = "mutindajulie2@gmail.com";
-    $email_subject = "Portfolio website message";
-    $email_body = "New message from $name \n";
-                   "Email address: $sender_email \n";
-                   "Message: $message";
-    $to = 'mutindajulie2@gmail.com';
-    $headers = "From $sender_email \r\n";
-    //send email
-    mail($to, $email_subject, $email_body, $headers);
-    //mail sent response
-    if(mail($to, $email_subject, $email_body, $headers)){
-        echo "Message sent successfully";
-    }
     else {
-        echo "Not sent";
-    }
-    //send success message for user
-    header('Location: thank-you.html');
-    /* //validate - injected input
-    function IsInjected($str) {
-        $injections = array('(\n+)',
-                    '(\r+)',
-                    '(\t+)',
-                    '(%0A+)',
-                    '(%0D+)',
-                    '(%08+)',
-                    '(%09+)'
-                    );
-        $inject = join('|', $injections);
-        $inject = "/$inject/i";
-        if(preg_match($inject,$str)) {
-            return true;
+        $name = $_POST['name'];
+        $sender_email = $_POST['email'];
+        $message = $_POST['message'];
+        //validate - invalid fields
+        if(empty($name)||empty($sender_email)) {
+            echo "Please fill in your name and email address";
+            exit;
+        }
+        //validate - injected input
+        if(IsInjected($visitor_email)) {
+        echo "Bad email value!";
+        exit;
+        }
+        //set variables
+        $recipient = "mutindajulie2@gmail.com";
+        $email_subject = "Portfolio website message";
+        $email_body = "New message from $name \n";
+                    "Email address: $sender_email \n";
+                    "Message: $message";
+        $to = 'mutindajulie2@gmail.com';
+        $headers = "From $sender_email \r\n";
+        //send email
+        mail($to, $email_subject, $email_body, $headers);
+        //mail sent response
+        if(mail($to, $email_subject, $email_body, $headers)){
+            echo "Message sent successfully";
         }
         else {
-            return false;
+            echo "Not sent";
         }
-        }
- */
+        //send success message for user
+        header('Location: thank-you.html');
+        /* //validate - injected input
+        function IsInjected($str) {
+            $injections = array('(\n+)',
+                        '(\r+)',
+                        '(\t+)',
+                        '(%0A+)',
+                        '(%0D+)',
+                        '(%08+)',
+                        '(%09+)'
+                        );
+            $inject = join('|', $injections);
+            $inject = "/$inject/i";
+            if(preg_match($inject,$str)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+            }
+    */
+    }
 ?>
