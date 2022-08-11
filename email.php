657da62d13a87c30a1fc76b00da85975ed412a11
Juliet <MsMutinda@users.noreply.github.com>
2022-08-11 16:06:37 +0300

    update august 2022

--- email.php
+++ email.php
@@ -1,65 +1,59 @@
 <?php
-//page cannot be accessed directly
-    if(!isset($_POST['submit'])) {
-        echo 'Error: You need to submit the form';
-    }
-    //collect the data
-    else {
-        $name = $_POST['name'];
-        $sender_email = $_POST['email'];
-        $message = $_POST['message'];
-        //validate - invalid fields
-        if(empty($name)||empty($sender_email)) {
-            echo "Please fill in your name and email address";
-            exit;
+    if(!empty($_POST)) { 
+        $errors = [];  
+        $errorMessage = '';  
+
+        //collect form data
+        $name = htmlspecialchars($_POST['name']);
+        $sender_email = htmlspecialchars($_POST['email']);
+        $message = htmlspecialchars($_POST['message']);
+
+        //validate
+        if(empty($name)) {
+            $errors[] = 'Please fill in your name';
         }
 
-        /* //validate - injected input
-        if(IsInjected($visitor_email)) {
-        echo "Bad email value!";
-        exit;
-        } */
+        if(empty($sender_email)) {
+            $errors[] = "Please fill in your email address";
+        }
+        else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors[] = 'The email address provided is invalid';
+        }
 
-        //set variables
-        $recipient = "mutindajulie2@gmail.com";
-        $email_subject = "Portfolio website message";
-        $email_body = "New message from $name \n";
-                    "Email address: $sender_email \n";
-                    "Message: $message";
-        $to = 'mutindajulie2@gmail.com';
-        $headers = "From $sender_email \r\n";
-        //send email
-        mail($to, $email_subject, $email_body, $headers);
-        //mail sent response
-        if(mail($to, $email_subject, $email_body, $headers)){
-            echo "Message sent successfully";
+        if(empty($message)) {
+            $errors[] = 'Please fill in your message';
         }
-        else {
-            echo "Not sent";
-        }
-        //send success message for user
-        header('Location: thank-you.html');
+
         
-        /* //validate - injected input
-        function IsInjected($str) {
-            $injections = array('(\n+)',
-                        '(\r+)',
-                        '(\t+)',
-                        '(%0A+)',
-                        '(%0D+)',
-                        '(%08+)',
-                        '(%09+)'
-                        );
-            $inject = join('|', $injections);
-            $inject = "/$inject/i";
-            if(preg_match($inject,$str)) {
-                return true;
+        if (!empty($errors)) {  //where some errors were noted
+            $allErrors = join('<br/>', $errors);
+            $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+        }
+        else {  //where there were no validation errors
+            //set variables
+            $email_subject = "Portfolio website message";
+            $email_body = "New message from $name \n";
+                        "Email address: $sender_email \n";
+                        "Message: $message";
+            $to = 'mutindajulie2@gmail.com';
+            $headers = "From $sender_email \r\n";
+
+            //send email & get response
+            $sendemail = mail($to, $email_subject, $email_body, $headers);
+            print_r($sendemail);
+
+            if($sendemail) {
+                echo 'Success: sent successfully!';
+                //clear the form
+                //unset($name, $sender_email, $message);
+                //redirect
+                //header('Location: thankyou.html');
+                //exit();
             }
             else {
-                return false;
+                echo 'Error: sent successfully!';
             }
-            }
-    */
+        }
 
     }
 ?>
